e997715a5fd0ee696f957730bdbf4727ce9e2cbd
Shubhojit Banik <sjbcs2023@gmail.com>
2026-05-24 22:57:24 +0600

    hash router fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
@@ -13,7 +13,7 @@ function App() {
 
     return (
 
-        <BrowserRouter>
+        <HashRouter>
 
             <Navbar />
 
@@ -43,7 +43,7 @@ function App() {
 
             </Routes>
 
-        </BrowserRouter>
+        </HashRouter>
 
     );
 
